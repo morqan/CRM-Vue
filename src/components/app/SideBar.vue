aa967921cca911735cab5ae268f2bb6ba91b4c85
morqan <qurbanbeyli.murad@gmail.com>
2021-02-15 03:21:56 +0400

    refactoring

---
+++ src/components/app/SideBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: value}">
+    <router-link
+    v-for="link in links"
+    :key="link.url"
+    tag="li"
+    active-class="active"
+    :to="link.url"
+    :exact="link.exact">
+      <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data: () => ({
+    links: [
+      { title: 'Score', url: '/', exact: true },
+      { title: 'History', url: '/history' },
+      { title: 'Planning', url: '/planning' },
+      { title: 'New record', url: '/record' },
+      { title: 'Categories', url: '/categories' }
+    ]
+  })
+}
+</script>
