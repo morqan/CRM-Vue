526380d8a86329385c32d30c7829857b891057bb
morqan <qurbanbeyli.murad@gmail.com>
2021-02-17 18:07:33 +0400

    vueliate

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate'
 import router from './router'
 import store from './store'
 import dateFilter from './filter/date.filter'
@@ -7,6 +8,7 @@ import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
+Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
 new Vue({
